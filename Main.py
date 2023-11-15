7236f58fb97f328b2253e2f38fa695b092816dd1
SergioSKA27 <sergioska81@hotmail.com>
2023-11-15 04:58:22 -0600

    Add page title and set page configuration, update content in actualtec.py and videos.py

--- Main.py
+++ Main.py
@@ -2,8 +2,8 @@ import streamlit as st
 from st_pages import Page, show_pages, add_page_title
 
 # Optional -- adds the title and icon to the current page
-add_page_title()
-
+#add_page_title()
+st.set_page_config(layout="wide", page_title="Desintegración de Partículas", page_icon=":books:", initial_sidebar_state="collapsed")
 # Specify what pages should be shown in the sidebar, and what their titles and icons
 # should be
 show_pages(
@@ -22,9 +22,36 @@ show_pages(
         Page("pages/Infografia.py","11. Infografía",":books:" ),
         Page("pages/actualtec.py","12. Relación del modelo con la tecnología actual",":books:" ),
         Page("pages/conclusiones.py","13. Conclusiones",":books:" ),
-        Page("pages/videos.py","14. Video",":books:" ),
+        Page("pages/videos.py","14. Video y Presentación",":books:" ),
         Page("pages/referencias.py","15. Referencias",":books:" ),
 
 
     ]
 )
+
+st.title("Inicio :house:")
+st.header("Desintegración de Partículas y el Modelo de Fermi.")
+st.divider()
+'''
+La desintegración beta es un fenómeno nuclear fascinante que ha sido objeto de estudio durante décadas.
+
+Este proceso, que implica la emisión de partículas beta y neutrinos de un núcleo atómico inestable, es un ejemplo de la
+interacción débil, una de las cuatro fuerzas fundamentales de la naturaleza. A través del estudio de la desintegración
+beta, los científicos han podido desentrañar muchos de los misterios de las partículas subatómicas y las fuerzas
+fundamentales que rigen el universo.
+
+
+El modelo de Fermi de la desintegración beta ha sido instrumental en este entendimiento. Proporciona el marco teórico
+que nos permite comprender cómo y por qué ocurre la desintegración beta, permitiendo a los científicos predecir la
+probabilidad de desintegración y la energía de las partículas emitidas. Además, el modelo de Fermi ha sido fundamental
+para el desarrollo de la teoría electrodébil, una parte esencial del Modelo Estándar de la física de partículas.
+
+Discutiremos cómo ocurre la desintegración beta, qué partículas se emiten y por qué, y cómo el modelo de Fermi nos ayuda
+a entender este proceso. También discutiremos las aplicaciones prácticas de la desintegración beta y el modelo de Fermi
+en la medicina, la energía y otras áreas.
+
+En resumen, este documento proporcionará una visión general completa y detallada de la desintegración beta y
+el modelo de Fermi. Esperamos que este documento sea de interés para cualquier persona interesada en la física de
+partículas, la tecnología nuclear o la ciencia en general.
+
+'''
--- pages/videos.py
+++ pages/videos.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-"Video de la infografía"
+#<div style="width: 100%;"><div style="position: relative; padding-bottom: 56.25%; padding-top: 0; height: 0;"><iframe title="FERMI" frameborder="0" width="1200" height="675" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://view.genial.ly/65515eb911fd730011cbe8d6" type="text/html" allowscriptaccess="always" allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe> </div> </div>
 
+st.set_page_config(layout="wide", page_title="Presentación", page_icon=":books:", initial_sidebar_state="collapsed")
+st.title("Presentación")
+st.divider()
+components.iframe("https://view.genial.ly/65515eb911fd730011cbe8d6", width=1200, height=675, scrolling=True)
 
+st.header("Video")
+st.divider()
+st.video("https://www.youtube.com/watch?v=Zz95_VvTxZM")
