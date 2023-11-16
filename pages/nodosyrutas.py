c0324430585d2edc5dfa1516ccb960e959d27948
Sergio Lopez Martinez <sergioska81@hotmail.com>
2023-11-16 12:01:35 -0600

    Update nodosyrutas.py

--- pages/nodosyrutas.py
+++ pages/nodosyrutas.py
@@ -1,4 +1,13 @@
 import streamlit as st
-
-
-'Nodos y Rutas'
+import graphviz
+st.graphviz_chart('''digraph {
+  Modelo -> BetaNegativa
+  Modelo -> BetaPositiva
+  BetaNegativa -> InicioNegativa
+  BetaNegativa -> ConservacionNegativa
+  BetaNegativa -> VerificacionNegativa
+  BetaPositiva -> InicioPositiva
+  BetaPositiva -> ConservacionPositiva
+  BetaPositiva -> VerificacionPositiva
+  Modelo -> Congruencia
+}''')
