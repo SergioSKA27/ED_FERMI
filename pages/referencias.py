c8b83bd1465d44a3f7178e4bfc009269b5730244
Sergio Lopez Martinez <sergioska81@hotmail.com>
2023-11-16 04:29:19 -0600

    Update referencias.py

--- pages/referencias.py
+++ pages/referencias.py
@@ -1,3 +1,50 @@
 import streamlit as st
+st.title("Referencias")
+st.divider()
+"""
+- Desintegración beta - Labster. (s. f.). https://theory.labster.com/es/betadecay/
+Krane, K. S., & Lynch, W. G. (1989). Introductory Nuclear Physics. Physics Today, 42(1),
+78. https://doi.org/10.1063/1.2810884
 
-"Referencias"
+- Colaboradores de Wikipedia. (2023, 16 octubre). Desintegración beta. Wikipedia, la
+enciclopedia libre. https://es.wikipedia.org/wiki/Desintegraci%C3%B3n_beta
+
+- Desintegración_Beta. (s. f.).
+https://www.quimica.es/enciclopedia/Desintegraci%C3%B3n_beta.html
+Libretexts. (2022, 2 noviembre). 7.2: decaimiento beta.
+
+- LibreTexts Español.
+https://espanol.libretexts.org/Fisica/F%C3%ADsica_Nuclear_y_de_Part%C3%ADculas/
+Libro%3A_Introducci%C3%B3n_a_la_F%C3%ADsica_Nuclear_Aplicada_(Cappellaro)/0
+7%3A_Desintegraci%C3%B3n_radiactiva_II/7.02%3A_Decaimiento_Beta#:~:text=La%
+20desintegraci%C3%B3n%20beta%20es%20una%20desintegraci%C3%B3n%20radiactiva%2
+0en,y%20una%20part%C3%ADcula%20casi%20sin%20masa%2C%20el%20neutrino.
+
+- Wikiwand - desintegración Beta. (s. f.). Wikiwand.
+https://www.wikiwand.com/es/Desintegraci%C3%B3n_beta
+ 
+- Griffiths, D. (2008). Introduction to Elementary Particles. Wiley-VCH.•
+Halzen, F., & Martin, A. D. (1984). Quarks and Leptons: An Introductory Course in Modern
+Particle Physics. Wiley.
+
+- Perkins, D. H. (2000). Introduction to High Energy Physics. Cambridge University Press.•
+Fermi, E. (1934). Versuch einer Theorie der β-Strahlen. I, Zeitschrift für Physik, 88(3-4),
+161-177.
+
+- MateFacil. (2018, 26 abril). 07. Desintegración radiactiva, ECUACIONES
+DIFERENCIALES [Vídeo]. YouTube. https://www.youtube.com/watch?v=0ccX_TIFiSA
+
+- Instituto de Física Teórica IFT. (2019, 8 febrero). ¿Cómo se desintegran las partículas?
+[Vídeo]. YouTube. https://www.youtube.com/watch?v=LM1OzwawVuM
+
+- Física Explicada. (2021, 24 febrero). Física 8.02 Ley Exponencial de la Desintegración
+Radiactiva. Actividad. datación por carbono-14. [Vídeo]. YouTube.
+https://www.youtube.com/watch?v=IJvCK41PJMM
+
+- Resolviendo matemáticas. (2023, 21 enero). Problema de desintegración radiactiva. [Vídeo].
+YouTube. https://www.youtube.com/watch?v=99KAXp7mTuE
+
+- Departamento de Física - Universidad de Burgos. (2020, 27 abril). Desintegración nuclear.
+Procesos alfa, beta y gamma - F. Apl. Salud S3 [Vídeo]. YouTube.
+https://www.youtube.com/watch?v=W4xtwCnN2-I
+"""
